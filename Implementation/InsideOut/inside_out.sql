e5b9db53cd131f85262e76210ca3730cea7edc6e
Nikola Nedeljkovic <nedeljkovic73@gmail.com>
2018-05-28 06:21:20 +0200

    Fixed sql file / added new plans

--- Implementation/InsideOut/inside_out.sql
+++ Implementation/InsideOut/inside_out.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 26, 2018 at 09:04 AM
+-- Generation Time: May 28, 2018 at 04:10 AM
 -- Server version: 5.7.20-log
 -- PHP Version: 5.6.35
 
@@ -34,16 +34,22 @@ CREATE TABLE IF NOT EXISTS `account` (
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `surname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `UNIQUEIDX1` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`email`, `name`, `surname`, `password`) VALUES
-('djordje.kostic@gmail.com', 'Djordje ', 'Kostic', 'direktor'),
-('nedeljkovic73@gmail.com', 'Nikola', 'Nedeljkovic', 'admin');
+INSERT INTO `account` (`email`, `name`, `surname`, `password`, `Id`) VALUES
+('djordje.kostic@gmail.com', 'Djordje ', 'Kostic', 'direktor', 1),
+('milan@gmail.com', 'Milan', 'Milanovic', 'menadzer', 2),
+('milinko@gmail.com', 'Milinko', 'Milinkovic', 'menadzer', 3),
+('nedeljkovic73@gmail.com', 'Nikola', 'Nedeljkovic', 'admin', 4),
+('nenadko@gmail.com', 'Nenad', 'Nenadkovic', 'radnik', 5),
+('ranko@gmail.com', 'Ranko', 'Rankovic', 'menadzer', 6);
 
 -- --------------------------------------------------------
 
@@ -84,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `company` (
 --
 
 INSERT INTO `company` (`companyName`, `numAccounts`, `numAccountsUsed`, `registrationLink`) VALUES
-('Novi Bunar', 10, 0, 'Y3DRFzVdR3tOkykAEzuw');
+('Novi Bunar', 10, 0, 'BVb7cVUBiWjUiOHdITbk');
 
 -- --------------------------------------------------------
 
@@ -122,6 +128,14 @@ CREATE TABLE IF NOT EXISTS `is_working` (
   KEY `R_15` (`teamName`,`companyName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `is_working`
+--
+
+INSERT INTO `is_working` (`email`, `teamName`, `companyName`) VALUES
+('nenadko@gmail.com', 'Tim3', 'Novi Bunar'),
+('nenadko@gmail.com', 'Tim4', 'Novi Bunar');
+
 -- --------------------------------------------------------
 
 --
@@ -135,6 +149,15 @@ CREATE TABLE IF NOT EXISTS `manager` (
   PRIMARY KEY (`email`),
   KEY `R_8` (`companyName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`email`, `companyName`) VALUES
+('milan@gmail.com', 'Novi Bunar'),
+('milinko@gmail.com', 'Novi Bunar'),
+('ranko@gmail.com', 'Novi Bunar');
 
 -- --------------------------------------------------------
 
@@ -179,6 +202,16 @@ CREATE TABLE IF NOT EXISTS `team` (
   KEY `R_21` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`teamName`, `numWorkers`, `numInProgressTasks`, `companyName`, `email`) VALUES
+('Tim1', 0, 0, 'Novi Bunar', 'milan@gmail.com'),
+('Tim2', 0, 0, 'Novi Bunar', 'ranko@gmail.com'),
+('Tim3', 0, 0, 'Novi Bunar', 'milan@gmail.com'),
+('Tim4', 0, 0, 'Novi Bunar', 'milinko@gmail.com');
+
 -- --------------------------------------------------------
 
 --
@@ -194,6 +227,13 @@ CREATE TABLE IF NOT EXISTS `worker` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Dumping data for table `worker`
+--
+
+INSERT INTO `worker` (`email`, `companyName`) VALUES
+('nenadko@gmail.com', 'Novi Bunar');
+
+--
 -- Constraints for dumped tables
 --
 
@@ -201,49 +241,49 @@ CREATE TABLE IF NOT EXISTS `worker` (
 -- Constraints for table `admin`
 --
 ALTER TABLE `admin`
-  ADD CONSTRAINT `R_12` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE;
+  ADD CONSTRAINT `R_12` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `director`
 --
 ALTER TABLE `director`
-  ADD CONSTRAINT `R_11` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE,
-  ADD CONSTRAINT `R_6` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE;
+  ADD CONSTRAINT `R_11` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `R_6` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `is_working`
 --
 ALTER TABLE `is_working`
-  ADD CONSTRAINT `R_14` FOREIGN KEY (`email`) REFERENCES `worker` (`email`) ON DELETE CASCADE,
-  ADD CONSTRAINT `R_15` FOREIGN KEY (`teamName`,`companyName`) REFERENCES `team` (`teamName`, `companyName`) ON DELETE CASCADE;
+  ADD CONSTRAINT `R_14` FOREIGN KEY (`email`) REFERENCES `worker` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `R_15` FOREIGN KEY (`teamName`,`companyName`) REFERENCES `team` (`teamName`, `companyName`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `manager`
 --
 ALTER TABLE `manager`
-  ADD CONSTRAINT `R_4` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE,
-  ADD CONSTRAINT `R_8` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE;
+  ADD CONSTRAINT `R_4` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `R_8` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `task`
 --
 ALTER TABLE `task`
-  ADD CONSTRAINT `R_19` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE,
-  ADD CONSTRAINT `R_20` FOREIGN KEY (`teamName`,`companyName`) REFERENCES `team` (`teamName`, `companyName`);
+  ADD CONSTRAINT `R_19` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `R_20` FOREIGN KEY (`teamName`,`companyName`) REFERENCES `team` (`teamName`, `companyName`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `team`
 --
 ALTER TABLE `team`
-  ADD CONSTRAINT `R_18` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE,
-  ADD CONSTRAINT `R_21` FOREIGN KEY (`email`) REFERENCES `manager` (`email`);
+  ADD CONSTRAINT `R_18` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `R_21` FOREIGN KEY (`email`) REFERENCES `manager` (`email`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `worker`
 --
 ALTER TABLE `worker`
-  ADD CONSTRAINT `R_5` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE,
-  ADD CONSTRAINT `R_7` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE;
+  ADD CONSTRAINT `R_5` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `R_7` FOREIGN KEY (`companyName`) REFERENCES `company` (`companyName`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
